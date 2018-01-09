2868ea365c8f2d5ed692b8e0c79a156c4ab6a571
Manerone <matheus.agio@hotmail.com>
2018-01-09 18:07:55 -0200

    wip: standard cracking n-dimensional

--- src/cracking/standard_cracking.h
+++ src/cracking/standard_cracking.h
@@ -10,6 +10,6 @@
 #include "avl_tree.h"
 #include "cracker_index.h"
 
-AvlTree standardCracking(IndexEntry*& c, int dataSize, AvlTree T, int lowKey, int highKey);
+AvlTree standardCracking(IndexEntry* c, int dataSize, AvlTree T, int lowKey, int highKey);
 
 #endif //MULTIDEMIONSIONALINDEXING_STANDARCRACKING_H
--- src/main.cpp
+++ src/main.cpp
@@ -54,64 +54,129 @@ bool verify_range_query(Column* c, RangeQuery* queries, size_t query_index, int6
     return true;
 }
 
-//int64_t scanQuery(IndexEntry *c, int64_t from, int64_t to){
-//    int64_t  sum = 0;
-//    for(int64_t i = from;i<=to;i++) {
-//        sum += c[i].m_key;
-//    }
-//
-//    return sum;
-//}
-//
-//void standardCracking(std::vector<double> * standardcrackingtime) {
-//    std::chrono::time_point<std::chrono::system_clock> start, end;
-//    std::chrono::duration<double> elapsed_seconds;
-//    RangeQuery rangequeries;
-//    loadQueries(&rangequeries,QUERIES_FILE_PATH,NUM_QUERIES);
-//    Column c;
-//    c.data = std::vector<int64_t>(COLUMN_SIZE);
-//    loadcolumn(&c,COLUMN_FILE_PATH,COLUMN_SIZE);
-//    start = std::chrono::system_clock::now();
-//    IndexEntry *crackercolumn = (IndexEntry *) malloc(COLUMN_SIZE * 2 * sizeof(int64_t));
-//    //Creating Cracker Column
-//    for (size_t i = 0; i < COLUMN_SIZE; i++) {
-//        crackercolumn[i].m_key = c.data[i];
-//        crackercolumn[i].m_rowId = i;
-//    }
-//    //Initialitizing Cracker Index
-//    AvlTree T = NULL;
-//    end = std::chrono::system_clock::now();
-//    standardcrackingtime->at(0) +=   std::chrono::duration<double>(end - start).count();
-//
-//    for (size_t i = 0; i < NUM_QUERIES; i++) {
-//        start = std::chrono::system_clock::now();
-//        //Partitioning Column and Inserting in Cracker Indexing
-//        T = standardCracking(crackercolumn , COLUMN_SIZE, T, rangequeries.leftpredicate[i], rangequeries.rightpredicate[i]);
-//        //Querying
-//        IntPair p1 = FindNeighborsGTE(rangequeries.leftpredicate[i], (AvlTree)T, COLUMN_SIZE-1);
-//        IntPair p2 = FindNeighborsLT(rangequeries.rightpredicate[i], (AvlTree)T, COLUMN_SIZE-1);
-//        int offset1 = p1->first;
-//        int offset2 = p2->second;
-//        free(p1);
-//        free(p2);
-//        int64_t sum = scanQuery(crackercolumn, offset1, offset2);
-//        end = std::chrono::system_clock::now();
-//        standardcrackingtime->at(i) += std::chrono::duration<double>(end - start).count();
-//#ifdef VERIFY
-//        bool pass = verify_range_query(c,rangequeries.leftpredicate[i],rangequeries.rightpredicate[i],sum);
-//                if (pass == 0) std::cout << "Query : " << i <<" " <<  pass << "\n";
-//#endif
-//
-//    }
-//    free(crackercolumn);
-//}
+std::vector<IndexEntry> scanQuery(IndexEntry *c, int64_t from, int64_t to){
+    std::vector<IndexEntry> result (to - from);
+    fprintf(stderr, "%d, %d, %d\n", to, from, result.size());
+    for(size_t i = from;i<=to;i++) {
+        result[i-from].m_key = c[i].m_key;
+        result[i-from].m_rowId = c[i].m_rowId;
+    }
+
+    return result;
+}
+
+int64_t join_results(std::vector<std::vector<IndexEntry>> partials){
+    int64_t sum = 0;
+    std::vector<int64_t> intersection (partials[0].size());
+    // Copy the first partial IDs
+    for (size_t j = 0; j < intersection.size(); ++j) {
+        intersection[j] = partials[0][j].m_rowId;
+    }
+
+    for (size_t i = 1; i < partials.size(); ++i) {
+        std::vector<int64_t> tmp_intersection;
+        for (size_t j = 0; j < partials[i].size(); ++j) {
+            int64_t id = partials[i][j].m_rowId;
+            // Check if id is inside intersection
+            if(std::find(intersection.begin(), intersection.end(), id) != intersection.end()){
+                tmp_intersection.push_back(id);
+            }
+        }
+        intersection = tmp_intersection;
+    }
+
+    // Find the resulting rows and sum them
+    for (size_t k = 0; k < intersection.size(); ++k) {
+        int64_t id = intersection[k];
+        for (size_t i = 1; i < partials.size(); ++i) {
+            for (size_t j = 0; j < partials[i].size(); ++j){
+                if(id == partials[i][j].m_rowId){
+                    sum += partials[i][j].m_key;
+                }
+            }
+        }
+
+    }
+    return sum;
+}
+
+void standardCracking(std::vector<double> * standardcrackingtime) {
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::duration<double> elapsed_seconds;
+    fprintf(stderr, "Column\n");
+    Column *c = (Column*) malloc(sizeof(Column) * NUMBER_OF_COLUMNS);
+    loadcolumn(c,COLUMN_FILE_PATH,COLUMN_SIZE, NUMBER_OF_COLUMNS);
+    fprintf(stderr, "Query\n");
+    RangeQuery *rangequeries = (RangeQuery *) malloc(sizeof(RangeQuery) * NUMBER_OF_COLUMNS);
+    loadQueries(rangequeries, QUERIES_FILE_PATH, NUM_QUERIES, NUMBER_OF_COLUMNS);
+    fprintf(stderr, "Index\n");
+    start = std::chrono::system_clock::now();
+    IndexEntry **crackercolumns = (IndexEntry **) malloc(NUMBER_OF_COLUMNS * sizeof(IndexEntry*));
+    for (size_t j = 0; j < NUMBER_OF_COLUMNS; ++j) {
+        crackercolumns[j] = (IndexEntry *) malloc(COLUMN_SIZE * sizeof(IndexEntry));
+        // Already create the cracker column
+        for (size_t i = 0; i < COLUMN_SIZE; ++i) {
+            crackercolumns[j][i].m_key = c[j].data[i];
+            crackercolumns[j][i].m_rowId = i;
+        }
+    }
+    //Initialitizing multiple Cracker Indexes
+    AvlTree *T = (AvlTree *) malloc(sizeof(AvlTree) * NUMBER_OF_COLUMNS);
+    for (size_t k = 0; k < NUMBER_OF_COLUMNS; ++k) {
+        T[k] = NULL;
+    }
+    end = std::chrono::system_clock::now();
+    standardcrackingtime->at(0) +=   std::chrono::duration<double>(end - start).count();
+
+    fprintf(stderr, "Doing queries\n");
+    for (size_t i = 0; i < NUM_QUERIES; i++) {
+        start = std::chrono::system_clock::now();
+        std::vector<std::vector<IndexEntry>> partial_results (NUMBER_OF_COLUMNS);
+        for (size_t j = 0; j < NUMBER_OF_COLUMNS; ++j) {
+            //Partitioning Column and Inserting in Cracker Indexing
+            fprintf(stderr, "StdCracking\n");
+            T[j] = standardCracking(
+                    crackercolumns[j],
+                    COLUMN_SIZE,
+                    T[j],
+                    rangequeries[j].leftpredicate[i],
+                    rangequeries[j].rightpredicate[i]
+            );
+
+            //Querying
+            fprintf(stderr, "Neighbors\n");
+            IntPair p1 = FindNeighborsGTE(rangequeries[j].leftpredicate[i], T[j], COLUMN_SIZE-1);
+            IntPair p2 = FindNeighborsLT(rangequeries[j].rightpredicate[i], T[j], COLUMN_SIZE-1);
+            int offset1 = p1->first;
+            int offset2 = p2->second;
+            free(p1);
+            free(p2);
+            fprintf(stderr, "Scan Query\n");
+            partial_results[j] = scanQuery(crackercolumns[j], offset1, offset2);
+        }
+        // Join the partial results and get sum
+        int64_t sum = join_results(partial_results);
+
+        end = std::chrono::system_clock::now();
+        standardcrackingtime->at(i) += std::chrono::duration<double>(end - start).count();
+
+#ifdef VERIFY
+        bool pass = verify_range_query(c,rangequeries, i, sum);
+        if (pass == 0) std::cout << "Query : " << i <<" " <<  pass << "\n";
+#endif
+    }
+    for (size_t l = 0; l < NUMBER_OF_COLUMNS; ++l) {
+        free(crackercolumns[l]);
+    }
+    free(crackercolumns);
+}
 
 long filterQuery3(IndexEntry **c, RangeQuery* queries, size_t query_index, int64_t from, int64_t to){
     int64_t sum = 0;
-    for (int i = from; i < to; ++i) {
+    for (size_t i = from; i < to; ++i) {
         bool is_valid = true;
         int64_t partial_sum = 0;
-        for (int j = 0; j < NUMBER_OF_COLUMNS && is_valid; ++j) {
+        for (size_t j = 0; j < NUMBER_OF_COLUMNS && is_valid; ++j) {
             int64_t keyL = queries[j].leftpredicate[query_index];
             int64_t keyH = queries[j].rightpredicate[query_index];
             if(!(c[j][i].m_key >= keyL && c[j][i] < keyH)){
@@ -145,9 +210,9 @@ void full_scan(std::vector<double> * fullscantime){
             crackercolumns[j][i].m_rowId = i;
         }
     }
-    for(int q=0;q<NUM_QUERIES;q++){
+    for(size_t q=0;q<NUM_QUERIES;q++){
         start = std::chrono::system_clock::now();
-        int64_t sum = filterQuery3(crackercolumns, rangequeries, q, 0, COLUMN_SIZE);
+        int64_t sum = filterQuery3(crackercolumns, rangequeries, q, 0, COLUMN_SIZE-1);
         end = std::chrono::system_clock::now();
         fullscantime->at(q) += std::chrono::duration<double>(end - start).count();
 #ifdef VERIFY
@@ -155,7 +220,7 @@ void full_scan(std::vector<double> * fullscantime){
         if (pass == 0) std::cout << "Query : " << q <<" " <<  pass << "\n";
 #endif
     }
-    for (int i = 0; i < NUMBER_OF_COLUMNS; ++i) {
+    for (size_t i = 0; i < NUMBER_OF_COLUMNS; ++i) {
         free(crackercolumns[i]);
     }
     free(crackercolumns);
@@ -229,18 +294,18 @@ int main(int argc, char** argv) {
         }
     }
 ////        STANDARD CRACKING
-//    else if (INDEXING_TYPE == 1){
-//        std::vector<double> standardcracking(NUM_QUERIES);
-//        for (int i = 0; i < NUMBER_OF_REPETITIONS; i++){
-//            fprintf(stderr, "Repetition #%d\n", i);
-//            standardCracking(&standardcracking);
-//        }
-//
-//        for (int q = 0; q < NUM_QUERIES; q++) {
-//            standardcracking[q] = standardcracking[q]/NUMBER_OF_REPETITIONS;
-//            std::cout  << standardcracking[q] << "\n";
-//        }
-//    }
+    else if (INDEXING_TYPE == 1){
+        std::vector<double> standardcracking(NUM_QUERIES);
+        for (int i = 0; i < NUMBER_OF_REPETITIONS; i++){
+            fprintf(stderr, "Repetition #%d\n", i);
+            standardCracking(&standardcracking);
+        }
+
+        for (int q = 0; q < NUM_QUERIES; q++) {
+            standardcracking[q] = standardcracking[q]/NUMBER_OF_REPETITIONS;
+            std::cout  << standardcracking[q] << "\n";
+        }
+    }
 //
 ////        FULL INDEX
 //    else if (INDEXING_TYPE == 2){
