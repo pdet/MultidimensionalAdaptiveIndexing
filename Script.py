a0048cedc208e1e8419298da82028b2c7dbf5fe2
Manerone <matheus.agio@hotmail.com>
2018-01-25 15:43:32 -0200

    wip

--- Script.py
+++ Script.py
@@ -14,13 +14,13 @@ FULL_KD_TREE = 4
 
 
 # Select Experiments to run
-experiments = [FULL_KD_TREE, FULL_SCAN]
+experiments = [KD_TREE]
 # Main Configurations
 COLUMN_FILE_PATH = "./column.txt"
 QUERIES_FILE_PATH = "./query.txt"
-NUM_QUERIES = '1000'
-NUMBER_OF_REPETITIONS = '5'
-COLUMN_SIZE = '100000'
+NUM_QUERIES = '100'
+NUMBER_OF_REPETITIONS = '1'
+COLUMN_SIZE = '10000'
 NUMBER_OF_COLUMNS = '5'
 
 SELECTIVITY_PERCENTAGE = "0.5"
@@ -33,15 +33,15 @@ COLUMN_PATTERN = RANDOM
 BPTREE_ELEMENTSPERNODE = '16384'  # Only used for Full Index
 
 # Saving Experiments
-if os.path.exists("experiments/") != 1:
-    os.system('mkdir experiments')
-experimentsList = os.listdir("experiments/")
+if os.path.exists("ScriptExperiments/") != 1:
+    os.system('mkdir ScriptExperiments')
+experimentsList = os.listdir("ScriptExperiments/")
 aux = 0
 for experiment in experimentsList:
     if aux < int(experiment):
         aux = int(experiment)
 currentexperiment = aux + 1
-PATH = "experiments/" + str(currentexperiment) + '/'
+PATH = "ScriptExperiments/" + str(currentexperiment) + '/'
 os.system('mkdir ' + PATH)
 os.system("cp Script.py " + PATH)
 
@@ -63,36 +63,36 @@ if os.system("./gendata" + " " + COLUMN_FILE_PATH + " " + QUERIES_FILE_PATH + " 
 if FULL_SCAN in experiments:
     print("Running Full Scan")
     if os.system("./crackingmain" + " " + COLUMN_FILE_PATH + " " + QUERIES_FILE_PATH + " " + NUM_QUERIES
-                 + " " + NUMBER_OF_REPETITIONS + " " + COLUMN_SIZE + " " + str(FULL_SCAN) + " " + str(NUMBER_OF_COLUMNS) + " >> " + PATH + "fs.txt") != 0:
+                 + " " + COLUMN_SIZE + " " + str(FULL_SCAN) + " " + str(NUMBER_OF_COLUMNS) + " >> " + PATH + "fs.txt") != 0:
         print("Running Failed")
         exit()
 
 if STANDARD_CRACKING in experiments:
     print("Running Standard Cracking")
     if os.system("./crackingmain" + " " + COLUMN_FILE_PATH + " " + QUERIES_FILE_PATH + " " + NUM_QUERIES
-                 + " " + NUMBER_OF_REPETITIONS + " " + COLUMN_SIZE + " " + str(STANDARD_CRACKING) + " " + str(NUMBER_OF_COLUMNS) + " >> " + PATH + "std.txt") != 0:
+                 + " " + COLUMN_SIZE + " " + str(STANDARD_CRACKING) + " " + str(NUMBER_OF_COLUMNS) + " >> " + PATH + "std.txt") != 0:
         print("Running Failed")
         exit()
 
 if FULL_INDEX in experiments:
     print("Running Full Index")
     if os.system("./crackingmain" + " " + COLUMN_FILE_PATH + " " + QUERIES_FILE_PATH + " " + NUM_QUERIES
-                 + " " + NUMBER_OF_REPETITIONS + " " + COLUMN_SIZE + " " + str(FULL_INDEX) + " " + str(NUMBER_OF_COLUMNS) + " " + BPTREE_ELEMENTSPERNODE + " >> " + PATH + "fi.txt") != 0:
+                 + " " + COLUMN_SIZE + " " + str(FULL_INDEX) + " " + str(NUMBER_OF_COLUMNS) + " " + BPTREE_ELEMENTSPERNODE + " >> " + PATH + "fi.txt") != 0:
         print("Running Failed")
         exit()
 
 if KD_TREE in experiments:
     print("Running KDTree")
     print("./crackingmain" + " " + COLUMN_FILE_PATH + " " + QUERIES_FILE_PATH + " " + NUM_QUERIES
-          + " " + NUMBER_OF_REPETITIONS + " " + COLUMN_SIZE + " " + str(KD_TREE) + " " + str(NUMBER_OF_COLUMNS))
+          + " " + COLUMN_SIZE + " " + str(KD_TREE) + " " + str(NUMBER_OF_COLUMNS))
     if os.system("./crackingmain" + " " + COLUMN_FILE_PATH + " " + QUERIES_FILE_PATH + " " + NUM_QUERIES
-                 + " " + NUMBER_OF_REPETITIONS + " " + COLUMN_SIZE + " " + str(KD_TREE) + " " + str(NUMBER_OF_COLUMNS) + " >> " + PATH + "kd.txt") != 0:
+                 + " " + COLUMN_SIZE + " " + str(KD_TREE) + " " + str(NUMBER_OF_COLUMNS) + " >> " + PATH + "kd.txt") != 0:
         print("Running Failed")
         exit()
 
 if FULL_KD_TREE in experiments:
     print("Running Full KDTree")
     if os.system("./crackingmain" + " " + COLUMN_FILE_PATH + " " + QUERIES_FILE_PATH + " " + NUM_QUERIES
-                 + " " + NUMBER_OF_REPETITIONS + " " + COLUMN_SIZE + " " + str(FULL_KD_TREE) + " " + str(NUMBER_OF_COLUMNS) + " >> " + PATH + "kdf.txt") != 0:
+                 + " " + COLUMN_SIZE + " " + str(FULL_KD_TREE) + " " + str(NUMBER_OF_COLUMNS) + " >> " + PATH + "kdf.txt") != 0:
         print("Running Failed")
         exit()
