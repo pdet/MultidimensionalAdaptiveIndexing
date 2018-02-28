e6da54ebad3ccfa0c108e6142caa6d7105634879
Pedro Holanda <pedroholanda@gmail.com>
2018-02-28 11:19:41 +0100

    Wrong number of semicolons

--- results/combine.py
+++ results/combine.py
@@ -3,7 +3,7 @@ import sys
 
 output = open('output.csv', 'w+')
 
-output.write('algorithm;repetition;query_selectivity;query_zipf;query_number;query_pattern;column_size;column_pattern;bptree_elementspernode;number_of_columns;index_creation;index_lookup;scan_time;total_time\n')
+output.write('algorithm;repetition;query_selectivity;query_zipf;query_number;query_pattern;column_size;column_pattern;bptree_elementspernode;number_of_columns;index_creation;index_lookup;scan_time;join_time;total_time\n')
 
 if len(sys.argv) > 1:
 	os.chdir(sys.argv[1])
@@ -21,11 +21,11 @@ def combine_results(directory = '../experimentresults/', base = ''):
 				for line in new_file:
 					if line[-1] != '\n':
 						continue
-					if line.count(';') == 14:
+					if line.count(';') == 13:
 						splits = line.split(';')
 						splits.insert(len(splits)-1, '')
 						line = ';'.join(splits)
-					if line.count(';') != 15:
+					if line.count(';') != 14:
 						continue
 					try:
 						float(line.split(';')[-1])
