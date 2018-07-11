4a78249d2fe455d67ab424fa804721bc90307549
Matheus Nerone <matheus.agio@hotmail.com>
2018-07-11 16:24:39 -0300

    Changing script to add one-sided-queries

--- benchmarks/UniformRandom/Script.py
+++ benchmarks/UniformRandom/Script.py
@@ -27,6 +27,8 @@ COLUMN_SIZE = '10000000'
 NUMBER_OF_COLUMNS = '8'
 KDTREE_THRESHOLD = '2000'  # Only used for KDTree
 
+ONE_SIDED_PERCENTAGE = '0'
+
 SELECTIVITY_PERCENTAGE = "0.2"
 QUERIES_PATTERN =  RANDOM 
 COLUMN_PATTERN = RANDOM  
@@ -91,7 +93,8 @@ if os.system('make') != 0:
 
 print("Generating Data")
 if os.system("./gendata --num-queries=" + NUM_QUERIES + " --column-size=" + COLUMN_SIZE + " --column-number=" +  NUMBER_OF_COLUMNS
- + " --selectivity=" +SELECTIVITY_PERCENTAGE + " --queries-pattern=" +  QUERIES_PATTERN + " --column-pattern="+ COLUMN_PATTERN) != 0:
+ + " --selectivity=" +SELECTIVITY_PERCENTAGE + " --queries-pattern=" +  QUERIES_PATTERN + " --column-pattern="+ COLUMN_PATTERN
+ + " --one-side-ranges=" +ONE_SIDED_PERCENTAGE) != 0:
     print("Generating Data Failed")
     exit()
 
