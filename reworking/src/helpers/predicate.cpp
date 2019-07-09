9ba722f1c284d86b325fcf98f58601a2116113eb
Matheus Nerone <matheus@cwi.nl>
2019-07-09 12:41:38 +0200

    Changing predicate column type

--- reworking/src/helpers/predicate.cpp
+++ reworking/src/helpers/predicate.cpp
@@ -2,15 +2,16 @@
 #define PREDICATE
 
 #include <cstdint>
+#include <cstddef>
 
 class Predicate
 {
 public:
     float low;
     float high;
-    float column;
+    size_t column;
 
-    Predicate(float low, float high, float column)
+    Predicate(float low, float high, size_t column)
         : low(low), high(high), column(column){}
     ~Predicate(){}
 };
