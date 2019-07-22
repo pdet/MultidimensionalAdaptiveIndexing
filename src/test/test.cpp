7587dd3155fe1a9d2ed228ded2b96f82362ffbe2
Matheus Nerone <matheus@cwi.nl>
2019-07-22 13:08:55 +0200

    On test.cpp, now baseline is separated from the other algorithms

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -15,14 +15,14 @@ int main(){
 
     auto algorithms = IndexFactory::allIndexes();
 
-    algorithms.push_back(IndexFactory::baseline_index());
+    auto baseline = IndexFactory::baseline_index();
 
     vector<vector<size_t>> result_sizes(algorithms.size());
 
     cout << "Testing algorithms: " << endl;
 
     for(size_t i = 0; i < algorithms.size(); ++i){
-        auto algorithm = move(algorithms.at(i));
+        auto algorithm = algorithms.at(i);
         cout << "   '-" << algorithm->name() << endl;
         algorithm->initialize(table);
         for(size_t j = 0; j < workload.size(); ++j){
@@ -32,20 +32,28 @@ int main(){
         }
     }
 
+    cout << "Running baseline..." << endl;
+
+    vector<size_t> baseline_results(workload.size());
+
+    baseline->initialize(table);
+    for(size_t j = 0; j < workload.size(); ++j){
+        baseline->adapt_index(workload.at(j));
+        auto result = baseline->range_query(workload.at(j));
+        baseline_results.at(j) = result->row_count();
+    }
+
     vector<size_t> number_of_different(algorithms.size(), 0);
 
     for(size_t query_index = 0; query_index < workload.size(); ++query_index){
         for(size_t alg_index = 0; alg_index < algorithms.size(); ++alg_index){
-            cout << result_sizes.at(alg_index).at(query_index) << " ";
-            if(result_sizes.at(alg_index).at(query_index) != result_sizes.at(0).at(query_index))
+            if(result_sizes.at(alg_index).at(query_index) != baseline_results.at(query_index))
                 number_of_different.at(alg_index)++;
         }
-        cout << endl;
     }
 
-    cout << "-----------------------" << endl;
     for(size_t alg_index = 0; alg_index < algorithms.size(); ++alg_index){
-            cout << number_of_different.at(alg_index) << " ";
+            cout << algorithms.at(alg_index)->name() <<": " << number_of_different.at(alg_index) << endl;
     }
     cout << endl;
     return 0;
