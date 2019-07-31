ba0027a42898920f38f184dc46b67db372592774
Matheus Nerone <matheus@cwi.nl>
2019-07-31 15:12:45 +0200

    Improving data generator

--- src/data/generator.cpp
+++ src/data/generator.cpp
@@ -9,27 +9,106 @@
 #include <vector>
 #include <unordered_map>
 
+// For the command line parsing
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #define FEATUREVECTORS_FILE "data/datasets/chr22_feature.vectors"
 #define GENES_FILE "data/datasets/genes.txt"
 #define POWER_DATASET_FILE "data/datasets/DEBS2012-ChallengeData.txt"
 
-#define DATA_FILE "data.sql"
-#define QUERY_FILE "queries.sql"
+#define DATA_FILE "data"
+#define QUERY_FILE "queries"
+
+void usage(){
+  std::cout << std::endl;
+  std::cout << "Usage:" <<std::endl;
+  std::cout << "-r <number_of_rows>" << std::endl;
+  std::cout << "-d <number_of_dimensions>" << std::endl;
+  std::cout << "-w <workload_choice>" << std::endl;
+  std::cout << "  '-(0=normal, 1=clustered, 2=uniform, 3=GMRQB, 4=power)" << std::endl;
+  std::cout << "  '- GMRQB has fixed selectivity and 19 dimensions." << std::endl;
+  std::cout << "  '- power has fixed 4 dimensions." << std::endl;
+  std::cout << "-s <selectivity>" << std::endl;
+  std::cout << "-q <number_of_queries>" << std::endl;
+  std::cout << "-t <query_type>" << std::endl;
+}
 
 int main(int argc, char* argv[]) {
-  if (argc < 4) {
-    std::cout << "Usage: " << argv[0] << " num_elements num_dimensions distribution(0=normal, 1=clustered, 2=uniform, 3=gmrqb, 4=power)" << std::endl;
-    return 1;
+  int64_t n_of_rows = -1;
+  int64_t dimensions = -1;
+  int64_t workload = -1;
+  float selectivity = -1;
+  int64_t number_of_queries = -1;
+  int64_t query_type = -1;
+
+  int c;
+  while ((c = getopt (argc, argv, "r:d:s:w:t:q:")) != -1){
+      switch (c)
+      {
+      case 'w':
+          workload = atoi(optarg);
+          break;
+      case 'r':
+          n_of_rows = atoi(optarg);
+          break;
+      case 'd':
+          dimensions = atoi(optarg);
+          break;
+      case 's':
+          selectivity = atof(optarg);
+          break;
+      case 'q':
+          number_of_queries = atoi(optarg);
+          break;
+      case 't':
+          query_type = atoi(optarg);
+          break;
+      default:
+          usage();
+          exit(-1);
+      }
   }
 
-  size_t n_of_rows = atoi(argv[1]);
-  size_t dimensions = atoi(argv[2]);
-  int o = 1;
-  float selectivity = 0.5;
+  if(n_of_rows == -1){
+    std::cout << "Errors:" << std::endl;
+    std::cout << "-r <n_of_rows> required" << std::endl;
+    usage();
+    exit(-1);
+  }
+  if(dimensions == -1){
+    std::cout << "Errors:" << std::endl;
+    std::cout << "-d <dimensions> required" << std::endl;
+    usage();
+    exit(-1);
+  }
+  if(workload == -1){
+    std::cout << "Errors:" << std::endl;
+    std::cout << "-w <workload> required" << std::endl;
+    usage();
+    exit(-1);
+  }
+  if(selectivity == -1){
+    std::cout << "Errors:" << std::endl;
+    std::cout << "-s <selectivity> required" << std::endl;
+    usage();
+    exit(-1);
+  }
+  if(number_of_queries == -1){
+    std::cout << "Errors:" << std::endl;
+    std::cout << "-q <number_of_queries> required" << std::endl;
+    usage();
+    exit(-1);
+  }
+  if(query_type == -1){
+    query_type = 7;
+    exit(-1);
+  }
 
-  if(atoi(argv[3]) == 3)
+  if(workload == 3)
     std::cout << "INFO: " << n_of_rows << " vectors, " << 19 << " dimensions." << std::endl;
-  else if(atoi(argv[3]) == 4)
+  else if(workload == 4)
     std::cout << "INFO: " << n_of_rows << " vectors, " << 4 << " dimensions." << std::endl;
   else
     std::cout << "INFO: " << n_of_rows << " vectors, " << dimensions << " dimensions." << std::endl;
@@ -37,7 +116,7 @@ int main(int argc, char* argv[]) {
   std::vector< std::vector<float> > data_points(n_of_rows, std::vector<float>(dimensions));
 
   // Get data or generate it
-  if (atoi(argv[3]) == 3) {
+  if (workload == 3) {
     // GMRQB
     dimensions = 19;
     size_t i = 0;
@@ -67,7 +146,7 @@ int main(int argc, char* argv[]) {
       i++;
     }
     myfile.close();
-  } else if(atoi(argv[3]) == 4) {
+  } else if(workload == 4) {
     // Power Dataset
     size_t i = 0;
     dimensions = 3;
@@ -99,7 +178,7 @@ int main(int argc, char* argv[]) {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::normal_distribution<double> nd(0.5,0.5);
-    std::uniform_real_distribution<double> ud(0,o);
+    std::uniform_real_distribution<double> ud(0,1);
     // create multi-modal distribution
     size_t distribution_modals = 3;
     std::vector<std::normal_distribution<double> > mmd(distribution_modals);
@@ -113,12 +192,12 @@ int main(int argc, char* argv[]) {
     for (size_t i = 0; i < n_of_rows; ++i) {
       std::vector<float> data_point(dimensions);
       for (size_t j = 0; j < dimensions; ++j) {
-        if (atoi(argv[3]) == 0)
+        if (workload == 0)
           data_point[j] = (float) nd(gen);
-        else if (atoi(argv[3]) == 1)
+        else if (workload == 1)
           data_point[j] = (float) mmd[rand() % distribution_modals](gen);
         else
-          data_point[j] = (float) ((rand() % ((int) o * 1000000)) / 1000000.0);
+          data_point[j] = (float) ((rand() % 1000000) / 1000000.0);
 
         myfile << data_point[j];
         if(j + 1 != dimensions)
@@ -132,11 +211,10 @@ int main(int argc, char* argv[]) {
   // random insertion order
   std::random_shuffle(data_points.begin(), data_points.end());
 
-  // Generate rq queries
-  int rq = 1000;
-  std::vector<std::vector<float> > lb_queries(rq, std::vector<float>(dimensions, std::numeric_limits<float>::min()));
-  std::vector<std::vector<float> > ub_queries(rq, std::vector<float>(dimensions, std::numeric_limits<float>::max()));
-  if (atoi(argv[3]) == 3) {
+  // Generate queries
+  std::vector<std::vector<float> > lb_queries(number_of_queries, std::vector<float>(dimensions, std::numeric_limits<float>::min()));
+  std::vector<std::vector<float> > ub_queries(number_of_queries, std::vector<float>(dimensions, std::numeric_limits<float>::max()));
+  if (workload == 3) {
     size_t i = 0;
     std::ifstream genes(GENES_FILE);
     std::string line;
@@ -145,7 +223,7 @@ int main(int argc, char* argv[]) {
     std::ofstream myfile(QUERY_FILE);
 
     std::vector<float> cols;
-    while (std::getline(genes, line) && i < rq) {
+    while (std::getline(genes, line) && i < number_of_queries) {
       cols.resize(0);
       std::vector<std::string> line_tokens;
       std::istringstream iss(line);
@@ -156,14 +234,18 @@ int main(int argc, char* argv[]) {
       ub_queries[i][5] = (float) 22;
       lb_queries[i][6] = (float) std::stof(line_tokens[4]) - 100000.0;
       ub_queries[i][6] = (float) std::stof(line_tokens[5]) + 200000.0;
-      int query_type = 7;//rand() % 8;
+      int query_type_;
+      if(query_type > 7)
+        query_type_ = rand() % 8;
+      else
+      query_type_ = query_type;
       int rand_point = rand() % n_of_rows;
 
       // myfile << "SELECT * FROM variations WHERE";
       // myfile << " chromosome = '" << lb_queries[i][5] << "'";
       // myfile << " AND location BETWEEN  '" << lb_queries[i][6] << "' AND '" << ub_queries[i][6] << "'";
 
-      switch (query_type) {
+      switch (query_type_) {
         // Query 2
         case 1:
           // qual (create range around a certain qual found in the data set)
@@ -406,16 +488,16 @@ int main(int argc, char* argv[]) {
     myfile.close();
   } else {
     std::ofstream myfile(QUERY_FILE);
-    for (size_t i = 0; i < rq; ++i) {
+    for (size_t i = 0; i < number_of_queries; ++i) {
       // myfile << "SELECT * FROM synthetic WHERE";
       int first  = rand() % n_of_rows;
       int second = rand() % n_of_rows;
       for (size_t j = 0; j < dimensions; ++j) {
         lb_queries[i][j] = std::min(data_points[first][j], data_points[second][j]);
         ub_queries[i][j] = std::max(data_points[first][j], data_points[second][j]);
-        if (atoi(argv[3]) == 2) {
-          lb_queries[i][j] = (float) ((rand() % (o * 10000)) / 1000000.0);
-          ub_queries[i][j] = lb_queries[i][j] + o*selectivity;
+        if (workload == 2) {
+          lb_queries[i][j] = (float) ((rand() % (10000)) / 1000000.0);
+          ub_queries[i][j] = lb_queries[i][j] + selectivity;
         }
       }
       for(size_t col = 0; col < dimensions; ++col){
