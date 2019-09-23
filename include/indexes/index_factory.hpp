78fecc0adc53b9f5cdc04e7fa7da9f29e31a77e3
Matheus Nerone <matheus@cwi.nl>
2019-09-23 15:06:02 +0200

    Fixing Median KD-Tree pointers

--- include/indexes/index_factory.hpp
+++ include/indexes/index_factory.hpp
@@ -5,7 +5,7 @@
 #include "full_scan.hpp"
 #include "cracking_kd_tree_broad.hpp"
 //#include "cracking_kd_tree_narrow.hpp"
-//#include "median_kd_tree.hpp"
+#include "median_kd_tree.hpp"
 //#include "average_kd_tree.hpp"
 //#include "quasii.hpp"
 
@@ -25,8 +25,8 @@ public:
             return make_unique<CrackingKDTreeBroad>();
        // // if(index_name == "Cracking KD-Tree Narrow")
        // //     return make_unique<CrackingKDTreeNarrow>();
-       // if(index_name == "KDTree-Median")
-       //     return make_unique<MedianKDTree>();
+        if(index_name == "KDTree-Median")
+            return make_unique<MedianKDTree>();
        // if(index_name == "KDTree-Average")
        //     return make_unique<AverageKDTree>();
        // if(index_name == "Quasii")
@@ -38,7 +38,7 @@ public:
         vector<shared_ptr<AbstractIndex>> indexes;
         indexes.push_back(make_unique<CrackingKDTreeBroad>());
         // indexes.push_back(make_unique<CrackingKDTreeNarrow>());
-        //indexes.push_back(make_unique<MedianKDTree>());
+        indexes.push_back(make_unique<MedianKDTree>());
         //indexes.push_back(make_unique<AverageKDTree>());
         // indexes.push_back(make_unique<Quasii>());
         return indexes;
