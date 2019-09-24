cc854475c0d7340ca00c84c90d9158b1d3c0c2c7
Matheus Nerone <matheus@cwi.nl>
2019-09-24 14:13:08 +0200

    Changing gitignore and run)me script

--- run_me.py
+++ run_me.py
@@ -1,18 +1,18 @@
 # Wizard to help generate the scripts for running experiments
-import subprocess
-from datetime import datetime
+import datetime
 from collections import OrderedDict
 import os
 import json
 import shutil
-import datetime
+
 
 class Benchmarks:
     Normal = 0
     Clustered = 1
-    UniformDistribution  = 2
+    UniformDistribution = 2
     GMRQB = 3
     Power = 4
+
 
 algs_hash = {
     '0': "Cracking KD-Tree Broad",
@@ -23,12 +23,20 @@ algs_hash = {
 }
 
 
+def pp_hash(h, prefix):
+    k = list(h.keys())
+    k.sort()
+    for key in k:
+        print(f"""{prefix}{key} -> {h[key]}""")
+
+
 def input_or_default(message, default):
     got = input(message)
     if len(got) == 0:
         return default
     else:
         return got
+
 
 def benchmark_string(benchmark):
     if benchmark == Benchmarks.Normal:
@@ -43,6 +51,7 @@ def benchmark_string(benchmark):
         return 'Power'
     exit('Error: Benchmark not identified')
 
+
 def number_to_algorithm(n):
     if(n in algs_hash):
         return algs_hash[n]
@@ -52,50 +61,68 @@ def number_to_algorithm(n):
         Available: {algs_hash}"""
     )
 
-########## WIZARD START ##########
+# @@@@@@@@@@ WIZARD START @@@@@@@@@@
 
 # Choose which benchmark to run
+
+
 benchmark = input_or_default(
-f"""*- Which benchmark do you want to execute?
+    f"""*- Which benchmark do you want to execute?
     [0] - Normal
     [1] - Clustered (Synthetic)
     [2] - Uniform Distribution (Synthetic)
     [3] - GMRQB
     [4] - Power
-Please type a number [default: {Benchmarks.Normal}]: """,
-Benchmarks.Normal
+    Please type a number [default: {Benchmarks.Normal}]: """,
+    Benchmarks.Normal
 )
 benchmark = int(benchmark)
 
 # All benchmarks have variable number of attributes
 # Except for Genome, which has always the same number
 if benchmark != Benchmarks.GMRQB:
-    number_of_attributes = input_or_default("""*- How many attributes [default: 5]? """, '5')
-    number_of_attributes = ''.join(number_of_attributes.split('.'))
+    number_of_attributes = input_or_default(
+            """*- How many attributes [default: 5]? """, '5'
+    )
+    number_of_attributes = ''.join(
+            number_of_attributes.split('.')
+    )
 else:
     number_of_attributes = 19
 number_of_attributes = int(number_of_attributes)
 
 # Number of tuples in the dataset
-number_of_tuples = input_or_default("""*- How many tuples [default: 1000]? """, '1000')
+number_of_tuples = input_or_default(
+        """*- How many tuples [default: 1000]? """, '1000'
+)
 number_of_tuples = ''.join(number_of_tuples.split('.'))
 number_of_tuples = int(number_of_tuples)
 
 # Number of queries to be executed
-number_of_queries = input_or_default("""*- How many queries [default: 1000]? """, '1000')
+number_of_queries = input_or_default(
+        """*- How many queries [default: 1000]? """, '1000'
+)
 number_of_queries = ''.join(number_of_queries.split('.'))
 number_of_queries = int(number_of_queries)
 
 # Query selectivity
 if benchmark == Benchmarks.GMRQB:
-    query_type = input_or_default("""*- Query type [default: 7]? For a mixed workload choose 8 or more:""", '7')
+    query_type = input_or_default(
+            """*- Query type [default: 7]\
+ For a mixed workload choose 8 or more:""",
+            '7'
+    )
     query_type = float(query_type)
 else:
     query_type = 7
 
 # Query selectivity
 if benchmark != Benchmarks.GMRQB:
-    selectivity = input_or_default("""*- Query selectivity [range (0, 1), default: 0.001]? """, '0.001')
+    selectivity = input_or_default(
+            """*- Query selectivity\
+[range (0, 1), default: 0.001]? """,
+            '0.001'
+    )
     selectivity = float(selectivity)
     if not (0 < selectivity < 1):
         raise Exception(
@@ -107,19 +134,22 @@ else:
 
 
 # Which algorithms to execute
+print("*- Which Algorithms do you want to compare?")
+pp_hash(algs_hash, "\t")
 algorithms = input_or_default(
-f"""*- Which Algorithms do you want to compare?
-    {algs_hash}
-Please specify splitting by spaces [default: 0]: """,
-'0'
+    f"""Please specify splitting by spaces [default: 0]: """,
+    '0'
 ).split(' ')
 algorithms = list(map(number_to_algorithm, OrderedDict.fromkeys(algorithms)))
 
-repetitions = input_or_default("How many times each algorithm should run [default: 3]?", "3")
+repetitions = input_or_default(
+        "How many times each algorithm should run [default: 3]?",
+        "3"
+)
 
-# Create runs dir
-if not os.path.exists('./runs/'):
-    os.makedirs('./runs/')
+# Create experiments dir
+if not os.path.exists('./experiments/'):
+    os.makedirs('./experiments/')
 
 # Name of experiment
 while(True):
@@ -128,10 +158,10 @@ while(True):
         f"""Please name your experiment [default: {date_now}]: """,
         date_now
     )
-    if os.path.exists('./runs/' + experiment_name):
+    if os.path.exists('./experiments/' + experiment_name):
         print("Experiment already exists, please choose another name.")
     else:
-        os.makedirs('./runs/' + experiment_name)
+        os.makedirs('./experiments/' + experiment_name)
         break
 
 # Create config dictionary
@@ -147,40 +177,42 @@ config = {
 }
 
 # Create config file
-with open('./runs/' + experiment_name + '/config.json', 'w') as file:
+with open('./experiments/' + experiment_name + '/config.json', 'w') as file:
     file.write(json.dumps(config, indent=4, separators=(',', ': ')))
 
-shutil.copy2('benchmark.py', './runs/' + experiment_name)
+shutil.copy2('benchmark.py', './experiments/' + experiment_name)
 
 # Summary of what was selected
 print(
-f"""
---- Summary ---
+    f"""
+    --- Summary ---
 
-Benchmark
-'- Benchmark: {benchmark} [{benchmark_string(benchmark)}]
-'- Number of Attributes: {number_of_attributes}
-'- Number of Tuples: {number_of_tuples}
-'- Number of Queries: {number_of_queries}
-'- Selectivity: {selectivity}
-'- Algorithms: {algorithms}
-'- Repetitions: {repetitions}
+    Benchmark
+    '- Benchmark: {benchmark} [{benchmark_string(benchmark)}]
+    '- Number of Attributes: {number_of_attributes}
+    '- Number of Tuples: {number_of_tuples}
+    '- Number of Queries: {number_of_queries}
+    '- Selectivity: {selectivity}
+    '- Algorithms: {algorithms}
+    '- Repetitions: {repetitions}
 
-You can find your experiment in:
--runs/{experiment_name}
-  '- benchmark.py               # python script to run the experiment
-  '- config.json          # configuration file
+    You can find your experiment in:
+    -experiments/{experiment_name}
+      '- benchmark.py               # python script to run the experiment
+      '- config.json          # configuration file
 
----------------
+    ---------------
 
-To run the program:
-    $ cd ./runs/{experiment_name}
-    $ python benchmark.py
+    To run the program:
+        $ cd ./experiments/{experiment_name}
+        $ python benchmark.py
 
-A SQLite file called 'results' will be created, with all the measurements stored inside.
-If you wish to move the script to some other place just make sure to fix the paths.
+    A SQLite file called 'results' will be created,
+        with all the measurements stored inside.
+    If you wish to move the script to some other place
+        just make sure to fix the paths.
 
-If you wish to visualize the results, call:
-    python visualizer.py $path to SQLite file$
-"""
+    If you wish to visualize the results, call:
+        python visualizer.py $path to SQLite file$
+    """
 )
