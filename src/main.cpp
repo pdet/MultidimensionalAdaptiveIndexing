45162b31ca053e0fa20c2b07338fb9c1407a0120
Matheus Nerone <matheus@cwi.nl>
2019-10-17 14:59:03 +0200

    fixing bug in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,7 @@ int main(int argc, char** argv){
             break;
         case 'p':
             config["minimum_partition_size"] = optarg;
+            break;
         default:
             cout << "Usage:\n";
             cout << "-w <workload_path>\n";
