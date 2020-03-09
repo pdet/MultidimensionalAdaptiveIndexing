ccedc452656f9e2657fa72bb56ca749577dcff95
Matheus Nerone <matheus@cwi.nl>
2020-03-09 16:30:39 +0100

    Fixing test name
    Test name was already taken, just changing it.

--- test/indexes/cracking_kd_tree_per_dimension.cpp
+++ test/indexes/cracking_kd_tree_per_dimension.cpp
@@ -3,7 +3,7 @@
 #include "cracking_kd_tree_per_dimension.hpp"
 
 //TEST_CASE(
-//        "Test Cracking KDTree\
+//        "Test CrackingKDTreePerDimension\
 //        [1000000 rows, 2 dimensions, 0.001 selectivity, 1000 queries]",
 //        "[CrackingKDTreePerDimension]" )
 //{
@@ -11,14 +11,14 @@
 //}
 //
 //TEST_CASE(
-//        "Test Cracking KDTree\
+//        "Test CrackingKDTreePerDimension\
 //        [1000000 rows, 16 dimensions, 0.001 selectivity, 1000 queries]",
 //        "[CrackingKDTreePerDimension]" )
 //{
 //    TestHelper::test(1000000, 16, 0.001, 1000, CrackingKDTreePerDimension::ID);
 //}
 TEST_CASE(
-        "Test Cracking KDTree\
+        "Test CrackingKDTreePerDimension\
         [10000 rows, 5 dimensions, 0.5 selectivity, 1000 queries]",
         "[CrackingKDTreePerDimension]" )
 {
@@ -26,7 +26,7 @@ TEST_CASE(
 }
 
 TEST_CASE(
-        "Test Cracking KDTree\
+        "Test CrackingKDTreePerDimension\
         [100000 rows, 2 dimensions, 0.1 selectivity, 1000 queries]",
         "[CrackingKDTreePerDimension]" )
 {
@@ -34,7 +34,7 @@ TEST_CASE(
 }
 
 TEST_CASE(
-        "Test Cracking KDTree\
+        "Test CrackingKDTreePerDimension\
         [100000 rows, 24 dimensions, 0.001 selectivity, 1000 queries]",
         "[CrackingKDTreePerDimension]" )
 {
